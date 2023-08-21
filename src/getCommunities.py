ab519cc771a4abfde96376219eb3f9aa89555a71
Ian Jackson <34674321+iajackson@users.noreply.github.com>
2023-08-21 20:06:44 +1000

    Add initial function definitions

---
+++ src/getCommunities.py
@@ -0,0 +1,24 @@
+"""
+getCommunities Action Module
+
+Calls organisations.v1.query
+
+Functions:
+    1. run(input): Calls organisations.v1.query
+
+Author: Ian Jackson
+"""
+
+
+def run(params):
+    """
+    organisations.v1.query
+
+    Parameters:
+        input (string): The function parameter(s) as a string
+
+    Returns:
+        string: The output of organisations.v1.query
+
+    """
+    return "Placeholder"
---
+++ src/startCollectors.py
@@ -0,0 +1,24 @@
+"""
+startCollectors Action Module
+
+Calls collectors.v1.process.start
+
+Functions:
+    1. run(input): Calls collectors.v1.process.start
+
+Author: Ian Jackson
+"""
+
+
+def run(params):
+    """
+    collectors.v1.process.start
+
+    Parameters:
+        input (string): The function parameter(s) as a string
+
+    Returns:
+        string: The output of collectors.v1.process.start
+
+    """
+    return "Placeholder"
